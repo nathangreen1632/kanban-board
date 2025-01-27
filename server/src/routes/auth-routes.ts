95082ab9e02b13b5103465a93a18deb07e8b8455
Nathan Green <ngreen1632@gmail.com>
2025-01-27 11:09:12 -0600

    (feat: auth) Implement user login route with JWT authentication

--- server/src/routes/auth-routes.ts
+++ server/src/routes/auth-routes.ts
@@ -2,14 +2,40 @@ import { Router, Request, Response } from 'express';
 import { User } from '../models/user.js';
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
+import dotenv from "dotenv";
+dotenv.config();
 
-export const login = async (req: Request, res: Response) => {
-  // TODO: If the user exists and the password is correct, return a JWT token
+export const login = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const { username , password } = req.body;
+
+    const user : User | null = await User.findOne({ where: { username } });
+    if (!user) {
+      res.status(401).json({ message: 'Invalid username' });
+      return;
+    }
+
+    const isPasswordValid : boolean = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      res.status(401).json({ message: 'Invalid password' });
+      return;
+    }
+
+    const token : string = jwt.sign(
+      { id: user.id, username: user.username},
+      process.env.JWT_SECRET_TOKEN as string,
+      { expiresIn: '30m' }
+    );
+
+    res.status(200).json({ token });
+  } catch (error) {
+    console.error('Login Error:', error);
+    res.status(500).json({ message: 'Something went wrong'});
+  }
 };
 
-const router = Router();
+const router: Router = Router();
 
-// POST /login - Login a user
 router.post('/login', login);
 
 export default router;

bc6714ae632d41afc210b04d3b76acbdde748190
Nathan Green <ngreen1632@gmail.com>
2025-01-27 11:23:26 -0600

    (feat: middleware) Add authentication token middleware to routes

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -6,7 +6,7 @@ import { authenticateToken } from '../middleware/auth.js';
 const router = Router();
 
 router.use('/auth', authRoutes);
-// TODO: Add authentication to the API routes
+router.use(authenticateToken);
 router.use('/api', apiRoutes);
 
 export default router;

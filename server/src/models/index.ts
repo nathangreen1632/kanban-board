bae18b2b97f0746614d829ab314c4b2e6c3caf1e
Nathan Green <ngreen1632@gmail.com>
2025-01-27 11:39:07 -0600

    (chore: typings) Add TypeScript type annotation for Sequelize instance

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -5,7 +5,7 @@ import { Sequelize } from 'sequelize';
 import { UserFactory } from './user.js';
 import { TicketFactory } from './ticket.js';
 
-const sequelize = process.env.DB_URL
+const sequelize : Sequelize = process.env.DB_URL
   ? new Sequelize(process.env.DB_URL)
   : new Sequelize(process.env.DB_NAME ?? '', process.env.DB_USER ?? '', process.env.DB_PASSWORD, {
       host: 'localhost',

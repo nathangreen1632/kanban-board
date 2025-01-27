15e6db6a64d64448c1a62a28981c99e8bc4662f3
Nathan Green <ngreen1632@gmail.com>
2025-01-26 21:35:29 -0600

    (feat: auth) implements login API request function

--- client/src/api/authAPI.tsx
+++ client/src/api/authAPI.tsx
@@ -1,9 +1,23 @@
 import { UserLogin } from "../interfaces/UserLogin";
 
 const login = async (userInfo: UserLogin) => {
-  // TODO: make a POST request to the login route
+  try {
+    const response : Response = await fetch('http://localhost:5000/api/auth/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(userInfo),
+    });
+    const data = await response.json();
+    if (!response.ok) {
+      throw new Error('invalid API response, check the network tab');
+    }
+    return data;
+  } catch (err) {
+    console.log('an error occurred', err);
+    return {};
+  }
 }
-
-
 
 export { login };

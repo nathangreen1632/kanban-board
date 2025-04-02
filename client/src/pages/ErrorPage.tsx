157b2bf588576bd4c6514d43f2601b23343a9d85
Nathan Green <ngreen1632@gmail.com>
2025-04-02 18:18:44 -0500

    Enhance styling for ErrorPage component
    Refactored ErrorPage to include improved styling with Tailwind CSS classes for better layout and responsiveness. Updated heading sizes, colors, and alignment to enhance user experience and visual appeal.

--- client/src/pages/ErrorPage.tsx
+++ client/src/pages/ErrorPage.tsx
@@ -1,8 +1,8 @@
 const ErrorPage = () => {
   return (
-    <section>
-      <h1>404: Page Not Found</h1>
-      <h1> ¯\_(ツ)_/¯</h1>
+    <section className="flex flex-col items-center justify-center min-h-screen text-center px-4">
+      <h1 className="text-4xl font-bold text-rose-600 mb-2">404: Page Not Found</h1>
+      <h2 className="text-2xl text-gray-600">¯\_(ツ)_/¯</h2>
     </section>
   );
 };

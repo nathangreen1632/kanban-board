8888e7aca17169d5065172c996ff67dfeb82ec87
Nathan Green <ngreen1632@gmail.com>
2025-04-02 18:17:27 -0500

    Refactor App layout for improved styling and structure
    Updated the app container with a responsive layout and modern styling using Tailwind CSS classes. Additionally, wrapped the navbar in a header element to enhance semantic structure. These changes improve both the visual presentation and maintainability of the code.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,18 @@
 import { Outlet } from 'react-router-dom';
-
 import Navbar from './components/Navbar';
 
 function App() {
-
   return (
-    <div className='container'>
-      <Navbar />
-      <main>
+    <div className="min-h-screen bg-gray-300 text-gray-900 flex flex-col">
+      <header className="px-4 py-6 shadow-md bg-gray-600">
+        <Navbar />
+      </header>
+      <main className="flex-1 w-full max-w-screen-xl mx-auto px-4 py-8">
         <Outlet />
       </main>
     </div>
-  )
+
+  );
 }
 
-export default App
+export default App;
